b252dd6eddbf3cf705c5c31eb17455d280349da0
pengrain <pengrunrain@163.com>
2016-04-25 10:17:38 +0800

    Initial Commit

---
+++ TestingMemory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestingMemory
+//
+//  Created by pengrun on 16/4/25.
+//  Copyright © 2016年 pengrun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
